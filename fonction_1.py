bd9dddf1e0f9dc83aed4c48b80b60285c577a63d
koueAnicet <anicetkoue@gmail.com>
2022-09-04 17:46:16 +0000

    fonction_1

--- fonction_1.py
+++ fonction_1.py
@@ -1,16 +1,17 @@
 print("*****THEOREME*******\n")
 
-c1= int(input('nombre1:\t'))
-c2= int(input('nombre2: \t'))
+hypo= int(input("Donnez l'hypothenus:\t"))
+c1= int(input('donnez un coté: \t'))
+from math import sqrt
+def pythagore(c1,hypo):
 
-def pythagore(c1,c2):
-
-    if c1  and c2 :
-        c3 =c2**2 + c1**2 
+    if c1 and hypo:
         
-        n= (c2**2 + c1**2)**(1/2)
-        if c3**2 == n:
-            return 'le coté est :' , int(c3)
-        return "cest pas un triangle retangle\n"
+        x = int(sqrt(hypo**2 - c1**2))
         
-print("\n",pythagore(c1,c2))
+        if hypo**2 == c1**2 + x**2:
+            print("\n #---C'est un triangle retangle---#")
+            return f"Le coté est : {x}."
+        return "C'est pas un triangle retangle\n"
+        
+print("\n",pythagore(c1,hypo))
